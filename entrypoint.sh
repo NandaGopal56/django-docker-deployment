2467dfd8f5991ab02f9c60792bd53595d9a439a4
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2024-06-20 00:08:51 +0530

    updated entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# collect all static files to the root directory
 python manage.py collectstatic --no-input
 
-gunicorn ecommerce_project.wsgi:application --bind 0.0.0.0:8000
+# start the gunicorn worker processws at the defined port
+gunicorn ecommerce_project.wsgi:application --bind 0.0.0.0:8000 &
+
+wait
